0da745b83ecc261ec264c75747de15a8be29fe36
Aly Hegazy <aly246974@gmail.com>
2026-05-17 14:20:53 +0300

    yea

---
+++ main2.py
@@ -0,0 +1,18 @@
+my_list=["A","B",["8","h","c"],["y,","banana","apple"],"orange","kiwi"]
+
+print(my_list)
+# print B
+i=1
+
+print(my_list[i])
+# print the 8,h,c
+i=2
+print(my_list[i])
+
+# print letter H from 8,h,c
+print(my_list[i][1])
+i=3
+print(my_list[i][1])
+#  printing kiwi
+i=5
+print(my_list[i])
